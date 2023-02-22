b7cc9599587026723091f10197745e19b0f4d978
Ajunku <ajunkubenedict07@gmail.com>
2023-02-22 06:16:28 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistdio.h>
+/**
+ * main - Entry point
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(2, änd that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	return (1);
+}
