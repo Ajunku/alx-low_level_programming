c344df5f93e609460c9f80fb2c3101a35d1d50dc
Ajunku <ajunkubenedict07@gmail.com>
2023-02-23 06:29:07 -0800

    2-print_alphabet_x10.c ...done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times
+ * print_alphabet_10 - Prints the alphabet 10 times.
  *
  * Return: Always 0 (Success)
  */
@@ -17,4 +17,5 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
+	return (0);
 }
