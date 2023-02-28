96100e9f69edab572deff6377f90a533bfd7e7e4
Ajunku <ajunkubenedict07@gmail.com>
2023-02-28 04:06:32 -0800

    0-reset_to_98.c update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
  * reset_to_98 - a function that takes a pointer to an int a as parameter
- * and updates then value it to to 98
+ * and updates then value it point to 98
  * @n: input
  * Return: n
  */
