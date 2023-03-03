9b09adb2d63ecc9020dd62144da1b0ae2db0880e
Ajunku <ajunkubenedict07@gmail.com>
2023-03-03 09:43:26 -0800

    finishing touches

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,12 +4,13 @@
  * add - adds two integers and returns the result
  * @a: int to be added to b
  * @b: int to be added to a
- * Returrn: sum of a and b
+ * Return: sum of a and b
  */
-
 int add(int a, int b)
 {
 	int c;
+
 	c = a + b;
+
 	return (c);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,10 +3,10 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
-
 void times_table(void)
 {
 	int i, j, k;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -16,20 +16,21 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-			if (k < 10 && j !=0)
+
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
-		}else if (k >= 10)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
 		}
-	}
 		_putchar('\n');
+	}
 }
